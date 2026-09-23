40a090c1f15f33394aeaf026f2c970bd31e1a7e6
waltermach2 <114309195+waltermach2@users.noreply.github.com>
2026-09-23 11:59:26 -0500

    Implement assignment operators examples in Python
    Added examples of assignment operators in Python.

---
+++ operadores-asignacion.py
@@ -0,0 +1,17 @@
+# Operadores Asignación
+
+x = 5
+x += 3 # Suma
+x -= 3 # Resta
+x *= 3 # Multiplicación
+x /= 3 # División
+x %= 2 # Módulo(resto)
+
+y = 20
+
+y //= 2 # División Entera
+y **= 3 # Exponente 10 * 10 * 10
+
+# WALRUS (morsa) :=
+print(z := 3)
+print(z)
