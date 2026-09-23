60bdca8c1e782706ec57577805eb8c5097c845e8
waltermach2 <114309195+waltermach2@users.noreply.github.com>
2026-09-23 12:00:04 -0500

    Implement comparison and logical operators examples
    Add comparison and logical operators examples in Python.

---
+++ operadores-logicos.py
@@ -0,0 +1,24 @@
+# Operadores de Comparación
+
+x = 5
+y = 3
+z = 5
+
+print(x == y) # ¿Si es igual? False
+print(x != y) # Si es distinto True
+print(x > z) # Es mayor a segunda True
+print(x < y) # Es menor False
+print(x >= z) # Es mayor o igual True
+print(x <= y) # Es menor o igual False
+
+# Operadores lógicos
+
+#      True      False
+print(x > y and y > z) # False
+print(x > y or y > z) # True
+
+v = True
+f = False
+
+print(not(v))
+print(not(f))
